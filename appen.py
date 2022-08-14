c00259a531b9838f12b4e12ccf710976f47cd7c8
maglom <magnuslomsdalen@gmail.com>
2022-08-14 11:54:32 +0200

    jada

--- appen.py
+++ appen.py
@@ -3,9 +3,23 @@ from flask import Flask
 
 app = Flask('__name__')
 
+html = '''
+<!DOCTYPE html>
+<html>
+<head>
+<title>Små brune bisker er de beste biskene</title>
+</head>
+<body>
+
+<h1>Dette er ubestridt</h1>
+<p>Ifølge folk.</p>
+
+</body>
+</html>'''
+
 @app.route('/')
 def index():
-    return 'Små brune bisker er de beste biskene'
+    return html
 
 if __name__ == "__main__":
     app.run()
